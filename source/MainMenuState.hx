7682361d18969245206a0b7fb2cdc1bcc42d44d6
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-06-19 03:24:47 +0700

    update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -338,13 +338,13 @@ class MainMenuState extends MusicBeatState
     var lerpScore:Int = 0;
 	var intendedScore:Int = 0;
 
-    	// USING THESE WEIRD VALUES SO THAT IT DOESNT FLOAT UP
-		sprDifficulty.y = leftArrow.y - 15;
-		intendedScore = Highscore.getWeekScore(curWeek, curDifficulty);
+	// USING THESE WEIRD VALUES SO THAT IT DOESNT FLOAT UP
+	sprDifficulty.y = leftArrow.y - 15;
+	intendedScore = Highscore.getWeekScore(curWeek, curDifficulty);
 
-		#if !switch
-		intendedScore = Highscore.getWeekScore(curWeek, curDifficulty);
-		#end
+	#if !switch
+	intendedScore = Highscore.getWeekScore(curWeek, curDifficulty);
+	#end
 
 	function changeItem(huh:Int = 0)
 	{
