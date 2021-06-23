148d9e8f326a5496530ac4077a56292e41449489
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-06-24 00:21:47 +0700

    update State.hx

---
+++ source/State.hx
@@ -0,0 +1,78 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+import lime.app.Application;
+
+class State extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+
+	private var bgColors:Array<String> = [
+		'#314d7f',
+		'#4e7093',
+		'#70526e',
+		'#594465'
+	];
+	private var colorRotation:Int = 1;
+
+	override function create()
+	{
+		super.create();
+		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('youmessedup', 'shared'));
+		bg.scale.x *= 1.55;
+		bg.scale.y *= 1.55;
+		bg.screenCenter();
+		add(bg);
+		
+		var kadeLogo:FlxSprite = new FlxSprite(FlxG.width, 0).loadGraphic(Paths.image('KadeEngineLogo'));
+		kadeLogo.scale.y = 0.3;
+		kadeLogo.scale.x = 0.3;
+		kadeLogo.x -= kadeLogo.frameHeight;
+		kadeLogo.y -= 180;
+		kadeLogo.alpha = 0.8;
+		add(kadeLogo);
+		
+		FlxTween.color(bg, 2, bg.color, FlxColor.fromString(bgColors[colorRotation]));
+		FlxTween.angle(kadeLogo, kadeLogo.angle, -10, 2, {ease: FlxEase.quartInOut});
+		
+		new FlxTimer().start(2, function(tmr:FlxTimer)
+		{
+			FlxTween.color(bg, 2, bg.color, FlxColor.fromString(bgColors[colorRotation]));
+			if(colorRotation < (bgColors.length - 1)) colorRotation++;
+			else colorRotation = 0;
+		}, 0);
+		
+		new FlxTimer().start(2, function(tmr:FlxTimer)
+		{
+			if(kadeLogo.angle == -10) FlxTween.angle(kadeLogo, kadeLogo.angle, 10, 2, {ease: FlxEase.quartInOut});
+			else FlxTween.angle(kadeLogo, kadeLogo.angle, -10, 2, {ease: FlxEase.quartInOut});
+		}, 0);
+		
+		new FlxTimer().start(0.8, function(tmr:FlxTimer)
+		{
+			if(kadeLogo.alpha == 0.8) FlxTween.tween(kadeLogo, {alpha: 1}, 0.8, {ease: FlxEase.quartInOut});
+			else FlxTween.tween(kadeLogo, {alpha: 0.8}, 0.8, {ease: FlxEase.quartInOut});
+		}, 0);
+	}
+
+	override function update(elapsed:Float)
+	{
+		if (controls.ACCEPT)
+		{
+			leftState = true;
+			trace('you are going to pay later.');
+		}
+		if (controls.BACK)
+		{
+		    FlxG.openURL('');
+		}
+		super.update(elapsed);
+	}
+}
